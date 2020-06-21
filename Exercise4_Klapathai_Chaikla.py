78c3891689aec493dc12ff4ca80c823974a7ae61
klapathai <klapathai@gmail.com>
2020-06-21 20:43:42 +0700

    Create Exercise4_Klapathai_Chaikla.py

---
+++ Exercise4_Klapathai_Chaikla.py
@@ -0,0 +1,15 @@
+'''
+Foundation English = eng
+General Business = busi
+Introduction to Computer Systems = introCom
+Computer Programming = com
+'''
+eng = 6.5
+busi = 8
+introCom = 10
+com = 9.5
+print("          ---Your Score---")
+print("Foundation English                : ",eng)
+print("General Business                  : ",busi)
+print("Introduction to Computer Systems  : ",introCom)
+print("Computer Programming              : ",com)
